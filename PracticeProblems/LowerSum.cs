8a3a08eb6cfbbf1b1ce8353e4fc2c5d43e01b106
Rithika L <rithika.l@tvsnext.com>
2023-07-23 17:25:32 +0530

    [Rithika] Add : Implemented

---
+++ PracticeProblems/LowerSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public class LowerSum
+    {
+        public void CalLowerSum()
+        {
+            Console.WriteLine("Enter size of array");
+            int n= Convert.ToInt32(Console.ReadLine());
+            int[] array= new int[n];
+            Console.WriteLine("Enter The Elements:");
+            for (int i= 0;i<array.Length;i++)
+            {
+                array[i]= Convert.ToInt32(Console.ReadLine());
+
+            }
+            int low1 = int.MaxValue;
+            int low2 = int.MaxValue;
+
+            foreach(int item in array)
+            {
+                if (item<low1)
+                {
+                    low2 = low1;
+                    low1 = item;
+                }
+                else if (item<low2)
+                {
+                    low2 = item;
+                }
+            }
+            int sum = low1 + low2;
+            Console.WriteLine("Two lowest numbers: " + low1 + " ," + low2);
+            Console.WriteLine("Sum of the two lowest numbers: " + sum);
+
+        }
+    }
+}
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -9,7 +9,7 @@ namespace PracticeProblems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter input to be executed\n 1.Find maximum and minimum element in array\n 2.Find count of duplicate elements\n 3.Print Unique Elements\n 4.Print Frequency of elements\n 5.Print Pattern\n 6.Print Pattern1\n 7.Print Reversed\n 8.Print Sum Of digits in number\n 9.Convert FirstWord to Capital");
+                Console.WriteLine("Enter input to be executed\n 1.Find maximum and minimum element in array\n 2.Find count of duplicate elements\n 3.Print Unique Elements\n 4.Print Frequency of elements\n 5.Print Pattern\n 6.Print Pattern1\n 7.Print Reversed\n 8.Print Sum Of digits in number\n 9.Convert FirstWord to Capital\n 10.Sum Of Lower Number");
                 int input = Convert.ToInt32(Console.ReadLine());
                 switch (input)
                 {
@@ -48,6 +48,10 @@ namespace PracticeProblems
                     case 9:
                         ConvertWord convertWord = new ConvertWord();
                         convertWord.ConvertWordoCapital();
+                        break;
+                    case 10:
+                        LowerSum lowerSum = new LowerSum();
+                        lowerSum.CalLowerSum();
                         break;
                     default:
                         flag = false;
