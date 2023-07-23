36ac7f22c85fdeb937fcc019a3e73e5f133f6627
Rithika L <rithika.l@tvsnext.com>
2023-07-23 13:35:54 +0530

    [Rithika] Add : Implemented

--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -9,7 +9,7 @@ namespace PracticeProblems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter input to be executed\n 1.Find maximum and minimum element in array\n 2.Find count of duplicate elements");
+                Console.WriteLine("Enter input to be executed\n 1.Find maximum and minimum element in array\n 2.Find count of duplicate elements\n 3.Print Unique Elements");
                 int input = Convert.ToInt32(Console.ReadLine());
                 switch (input)
                 {
@@ -21,7 +21,10 @@ namespace PracticeProblems
                         DuplicateElements duplicateElements = new DuplicateElements();
                         duplicateElements.CountDuplicateElements();
                         break;
-
+                    case 3:
+                        UniqueElements uniqueElements = new UniqueElements();
+                        uniqueElements.FindUniqueElements();
+                        break;
                     default:
                         flag = false;
                         break;
---
+++ PracticeProblems/UniqueElements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public class UniqueElements
+    {
+        public void FindUniqueElements()
+        {
+            Console.WriteLine("Enter Size of array");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int[] array= new int[n];
+            Console.WriteLine("Enter The Elements:");
+            for(int i=0;i<array.Length;i++)
+            {
+                array[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            HashSet<int> uniqueElements = new HashSet<int>();
+            foreach (int element in array)
+            {
+                if (!uniqueElements.Contains(element))
+                {
+                    uniqueElements.Add(element);
+                }
+            }
+            Console.WriteLine("Unique elements:");
+            foreach (int element in uniqueElements)
+            {
+                Console.WriteLine(element + " ");
+            }
+        }
+    }
+}
